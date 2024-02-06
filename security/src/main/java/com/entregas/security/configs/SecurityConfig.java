7a63472dac3a2e7d0c6a0f34cf1dec89b4de5840
Kleiser Nairobi <kleiser.nairobi@gmail.com>
2024-02-05 21:52:12 -0300

    adicionando usuários para teste

--- security/src/main/java/com/entregas/security/configs/SecurityConfig.java
+++ security/src/main/java/com/entregas/security/configs/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -46,5 +48,9 @@ public class SecurityConfig {
                 .build();
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- security/src/main/java/com/entregas/security/configs/userConfig/InitialUserInfo.java
+++ security/src/main/java/com/entregas/security/configs/userConfig/InitialUserInfo.java
@@ -1,4 +1,44 @@
 package com.entregas.security.configs.userConfig;
 
-public class InitialUserInfo {
+import com.entregas.security.entities.UserInfoEntity;
+import com.entregas.security.repositories.UserInfoRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class InitialUserInfo implements CommandLineRunner {
+
+    private final UserInfoRepo userInfoRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        UserInfoEntity manager = new UserInfoEntity();
+        manager.setUserName("Manager");
+        manager.setPassword(passwordEncoder.encode("password"));
+        manager.setRoles("ROLE_MANAGER");
+        manager.setEmailId("manager@manager.com");
+
+        UserInfoEntity admin = new UserInfoEntity();
+        admin.setUserName("Admin");
+        admin.setPassword(passwordEncoder.encode("password"));
+        admin.setRoles("ROLE_ADMIN");
+        admin.setEmailId("admin@admin.com");
+
+        UserInfoEntity user = new UserInfoEntity();
+        user.setUserName("User");
+        user.setPassword(passwordEncoder.encode("password"));
+        user.setRoles("ROLE_USER");
+        user.setEmailId("user@user.com");
+
+        userInfoRepo.saveAll(List.of(manager, admin, user));
+    }
+
 }
